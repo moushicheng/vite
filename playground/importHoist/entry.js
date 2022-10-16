4af3b0f6f5fd2e34e5718910aced76e4852b685e
moushicheng <1163675107@qq.com>
2022-10-16 13:36:26 +0800

    feat: add entry

--- playground/importHoist/entry.js
+++ playground/importHoist/entry.js
@@ -1,2 +1,10 @@
-path.resolve('server.js')
+console.log('\n\n----------1--------')
+path.resolve('1111')
 import path from 'node:path'
+import fs from 'node:fs'
+console.log('----------2--------')
+import('fs').then((res) => {
+  console.log('success')
+})
+console.log('----------3--------')
+path.resolve('server.js')
